729c616998e600cf5729e24b9b5b124a4b297529
Joel Frederico <458871+joelfrederico@users.noreply.github.com>
2019-10-14 20:02:06 -0700

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/joelfrederico/bprof',
-    version='0.1.4',
+    version='0.1.5',
     zip_safe=False,
     ext_modules=[module1]
 )
